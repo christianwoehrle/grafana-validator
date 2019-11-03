de830cda5d8aa6d55bc853dacff9e61b30138b31
Christian Woehrle <christian.woehrle@dwpbank.de>
2019-11-03 16:13:29 +0100

    start this stuff !

--- deploy/run-test.sh
+++ deploy/run-test.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env sh
 #docker run -it --rm christianwoehrle/postgres-validator -env URL_MASTER=ce-postgres -env URL_REPLICA=ce-postgres-repl
 
-export POSTGRES_DB_NAME="${POSTGRES_DB_NAME:-ce-postgres}"
-NAMESPACE="${NAMESPACE:-microservice-ce}"
+NAMESPACE="${NAMESPACE:-monitoring}"
 
-envsubst < test.yaml  | kubectl delete -n ${NAMESPACE} -f -
-#   2> /dev/null
-envsubst < test.yaml  | kubectl apply  -n ${NAMESPACE} -f -
+kubectl delete -n ${NAMESPACE} -f test.yaml 2> /dev/null
+kubectl apply  -n ${NAMESPACE} -f test.yaml
 
 echo "echo show logs in a second, exist with ctrl-c"
 sleep 1
@@ -21,6 +19,6 @@ sleep 1
 echo "."
 sleep 1
 echo "=================="
-kubectl logs -f -ljob-name=postgres-test-${POSTGRES_DB_NAME} --all-containers=true
+kubectl logs -f -ljob-name=grafnaostgres-test-${POSTGRES_DB_NAME} --all-containers=true
 
 
